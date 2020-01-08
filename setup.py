30f1e8b14ea9b928dd27c4d35f18f4491838f997
meichthys <mattbju2013@gmail.com>
2020-01-08 00:39:24 -0500

    update to work with pipenv

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="nextcloudmonitor",
-    version="0.0.2",
+    version="0.0.3",
     description="Python wrapper around nextcloud monitor api",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    py_modules=["nextcloudmonitor"],
 )
