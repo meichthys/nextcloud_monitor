9d5045bcd65f99ed7bdc078a207e3ae4f84f2803
meichthys <mattbju2013@gmail.com>
2020-01-07 19:28:14 -0500

    add setup.py for package deployment

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="nextcloudmonitor",
+    version="0.0.1",
+    description="Python wrapper around nextcloud monitor api",
+    packages=setuptools.find_packages(),
+)
