bc45858e36aa606b58b0486a512fe733944dc5a2
meichthys <mattbju2013@gmail.com>
2020-01-07 20:00:31 -0500

    update version and add readme to pypi package description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setuptools.setup(
     name="nextcloudmonitor",
-    version="0.0.1",
+    version="0.0.2",
     description="Python wrapper around nextcloud monitor api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
 )
