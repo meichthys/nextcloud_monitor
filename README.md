70b09df160ebfc1fae8333a6ea5cf978ef3120f2
meichthys <10717998+meichthys@users.noreply.github.com>
2020-11-15 10:30:52 -0500

    allow bypass of ssl_verification

--- README.md
+++ README.md
@@ -23,3 +23,9 @@ Notes:
 
 - The user must be a user that has access to the nextcloud monitor api (generally an admin user)
 - The nextcloud app password should be generated from the nextcoud security settings page.
+
+## Change Log
+
+- v1.0.0 : Initial Release
+- v1.1.0 : Added basic error handling for api errors
+- v1.2.0 : Allow ssl verification bypass
--- nextcloudmonitor.py
+++ nextcloudmonitor.py
@@ -11,20 +11,24 @@ class NextcloudMonitor:
         nextcloud_url (str): Full https url to a nextcloud instance
         user (str): Username of the Nextcloud user with access to the monitor api
         app_password (str): App password generated from Nextcloud security settings page
+        verify_ssl (bool): Allow bypassing ssl verification, but verify by default
     """
 
-    def __init__(self, nextcloud_url, user, app_password):
+    def __init__(self, nextcloud_url, user, app_password, verify_ssl=True):
         self.data = dict()
         self.api_url = (
             f"{nextcloud_url}/ocs/v2.php/apps/serverinfo/api/v1/info?format=json"
         )
         self.user = user
         self.password = app_password
+        self.verify_ssl = verify_ssl
         self.update()
 
     def update(self):
         try:
-            response = requests.get(self.api_url, auth=(self.user, self.password))
+            response = requests.get(
+                self.api_url, auth=(self.user, self.password), verify=self.verify_ssl
+            )
             self.data = response.json()["ocs"]["data"]
         except:
             raise NextcloudMonitorError(
