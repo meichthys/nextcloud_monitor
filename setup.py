8f7a5a8b4370434395a9d20db0da2e18a14e0d5a
meichthys <10717998+meichthys@users.noreply.github.com>
2021-11-05 20:32:29 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="nextcloudmonitor",
-    version="1.2.0",
+    version="1.2.1",
     description="Python wrapper around nextcloud monitor api",
     long_description=long_description,
     long_description_content_type="text/markdown",
